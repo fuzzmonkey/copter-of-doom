3bc34ee4f2150dc0a63c7adbb51d7f52930d02a0
Ben Gillies <bengillies@gmail.com>
2013-10-20 14:02:16 +0100

    Add Sphinx jars

--- speechr/project.clj
+++ speechr/project.clj
@@ -9,5 +9,4 @@
                  [clj-http "0.7.2"]
                  [com.cemerick/url "0.0.7"]
                  [org.clojure/data.json "0.2.2"]
-                 [facts/speech-recognition "1.0.0"]
-                 [edu.cmu.sphinx/sphinx4 "1.0-beta6"]])
+                 [facts/speech-recognition "1.0.0"]])
