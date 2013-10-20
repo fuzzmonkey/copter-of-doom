6b1af1ace84f197026ddc2b397f08182d1e9227f
Ben Gillies <bengillies@gmail.com>
2013-10-20 14:32:12 +0100

    Connect the speech app to the node app

--- speechr/src/speechr/core.clj
+++ speechr/src/speechr/core.clj
@@ -1,16 +1,36 @@
 (ns speechr.core
-  (:require [clojure.string :as string])
+  (:require [clojure.string :as string]
+            [clj-http.client :as http])
   (:import [edu.cmu.sphinx.frontend.util Microphone]
            [edu.cmu.sphinx.recognizer Recognizer]
            [edu.cmu.sphinx.result Result]
            [edu.cmu.sphinx.util.props ConfigurationManager])
   (:gen-class))
 
+(def node-server "http://localhost:3000/")
+
+(defn send-command [cmd value]
+  (http/post (str node-server "commands")
+             {:form-params {:cmd cmd
+                            :value value}}))
+
+(def commands {"take off" ["takeoff" nil]
+               "land" ["land" nil]
+               "right" ["clockwise" 0.5]
+               "down" ["down" 0.5]
+               "up" ["up" 0.5]
+               "left" ["counterClockwise" 0.5]
+               "forwards" ["front" 0.1]
+               "backwards" ["back" 0.1]
+               "stop" ["stop" nil]})
+
 (defn get-text [recognizer]
   (loop []
     (let [result (some-> recognizer .recognize .getBestFinalResultNoFiller)]
       (if-not (string/blank? result)
-        (println (str "you said: " result))
+        (do
+          (println (str "you said: " result))
+          (apply send-command (get commands result)))
         (println "dude, wtf? Speak up!"))
       (recur))))
 
